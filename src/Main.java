1e694d536ca598c3672a2b45c4cb8b9f8251556a
Mattys <mattysgervais@gmail.com>
2019-10-23 11:01:49 -0400

    Menu done, photoshop reste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -10,5 +16,40 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        Menu fichiers = new Menu("Fichiers");
+        Menu chargerImage = new Menu("Charger une image");
+        MenuItem i1 = new MenuItem("Image #1");
+        MenuItem i2 = new MenuItem("Image #2");
+        MenuItem i3 = new MenuItem("Image #3");
+
+        i1.setOnAction(event -> {});
+        i2.setOnAction(event -> {});
+        i3.setOnAction(event -> {});
+
+        chargerImage.getItems().addAll(i1,i2,i3);
+        fichiers.getItems().add(chargerImage);
+        Menu actions = new Menu("Actions");
+        MenuItem reint = new MenuItem("Réintialiser");
+
+        actions.getItems().add(reint);
+        MenuBar menu = new MenuBar(fichiers,actions);
+        BorderPane menuHaut = new BorderPane();
+        menuHaut.setTop(menu);
+
+        ContextMenu cm = new ContextMenu(fichiers,actions);
+
+        Group root = new Group(menuHaut);
+        Scene photoshop = new Scene(root);
+        photoshop.setOnContextMenuRequested(event -> cm.show(root,event.getScreenX(),event.getScreenY()));
+        reint.setOnAction(event->{primaryStage.setScene(photoshop);});
+
+
+        primaryStage.setMaxHeight(1080);
+        primaryStage.setMaxWidth(1920);
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(600);
+        primaryStage.setFullScreen(true);
+        primaryStage.setScene(photoshop);//TODO <3
+        primaryStage.show();
     }
 }
